3853e4143bd742e64319e66b083bed3132567cb3
teterkin <teterkin@gmail.com>
2017-12-30 22:06:09 +0500

    J2 HW4: Bot added + some Refactoring + ai selected by default + lineWrap in textArea + main windows icon added + windows listener which do nothing yet.

--- HW4/src/SimpleChat.java
+++ HW4/src/SimpleChat.java
@@ -1,9 +1,9 @@
+import bot.SimpleBot;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
+import java.io.*;
 
 public class SimpleChat
 {
@@ -15,7 +15,10 @@ public class SimpleChat
     private JPanel bottomPanel;
 
     private boolean textFieldClicked = false;
-    private boolean justSent = false;
+
+    SimpleBot bot = new SimpleBot();
+
+    static final String FILE_NAME = "chat_log.txt";
 
     // Constructor
     public SimpleChat()
@@ -29,24 +32,9 @@ public class SimpleChat
             {
                 super.keyReleased(e);
 
-                if (justSent)
-                {
-                    prepareForTextEntry();
-                    text.setText(String.valueOf(e.getKeyChar()));
-                    justSent = false;
-                }
-
                 if (e.getKeyCode() == KeyEvent.VK_ENTER)
                 {
-                    if (text.getText().trim().length() > 0)
-                    {
-                        if (!text.getText().equals("Введите текст..."))
-                        {
-                            textArea.append("\n" + text.getText());
-                            clean();
-                            justSent = true;
-                        }
-                    }
+                    sendText();
                 }
             }
         });
@@ -63,12 +51,58 @@ public class SimpleChat
                 }
             }
         });
+        button.addMouseListener(new MouseAdapter()
+        {
+            @Override
+            public void mouseReleased(MouseEvent e)
+            {
+                super.mouseReleased(e);
+                sendText();
+            }
+        });
     } // End of Constructor
+
+    private void sendText()
+    {
+        if (text.getText().trim().length() > 0)
+        {
+            if (!text.getText().equals("Введите текст..."))
+            {
+
+                String myText = "\nВы: " + text.getText();
+                textArea.append(myText);
+                logText(myText);
+
+                String botText = "\nБот: " +
+                        bot.sayInReturn(text.getText(), ai.isSelected());
+                textArea.append(botText);
+                logText(botText);
+
+                clean();
+            }
+        }
+    }
+
+    private void logText(String text)
+    {
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(FILE_NAME, true), "utf-8"))) {
+            writer.write("\n" + text);
+        } catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
     private void clean()
     {
-        text.setText("Введите текст...");
-        text.setForeground(Color.gray);
+        text.setText("");
         textFieldClicked = false;
     }
 
@@ -79,18 +113,27 @@ public class SimpleChat
         textFieldClicked = true;
     }
 
-
-
+    // main
     public static void main(String[] args)
     {
         final int START_LOCATION = 200;
         final int WINDOW_WIDTH = 350;
         final int WINDOW_HEIGHT = 450;
-
         JFrame frame = new JFrame("Телекилограм");
+        frame.setIconImage(new ImageIcon(SimpleChat.class.getResource("images/SendButton.PNG")).getImage());
         frame.setContentPane(new SimpleChat().panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setBounds(START_LOCATION, START_LOCATION, WINDOW_WIDTH, WINDOW_HEIGHT);
+        frame.addWindowListener(new WindowAdapter()
+        {
+            @Override
+            public void windowClosing(WindowEvent e)
+            {
+                super.windowClosing(e);
+                // Пока!
+            }
+        });
         frame.setVisible(true);
     }
+
 }
