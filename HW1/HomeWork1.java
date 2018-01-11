24e426be906744f942ff178e24e9d66be6a73508
teterkin <teterkin@gmail.com>
2018-01-12 00:58:38 +0500

    J2 HW1: Refactorng. Changed Folder name and Class name.

--- HW1/HomeWork1.java
+++ HW1/HomeWork1.java
@@ -4,13 +4,14 @@ import team.*;
 import course.*;
 
 /**
- * Write a description of class J2Lesson1 here.
+ * Write a description of class HomeWork1 here.
  *
  * @author Sergey Iryupin, Alexander Teterkin
  * @version dated Dec 21, 2017
  * @link https://github.com/
  */
-public class J2Lesson1 {
+public class HomeWork1
+{
     
     public static void main(String[] args) {
         Animal[] zoo = {new Cat("Мурзик"), new Hen("Иззи"), new Hippo("Гипоппо"), new Hen("Огненая птица")};
